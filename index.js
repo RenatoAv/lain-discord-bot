ba7ed25654f84eec743eac2d3c00605b6ec34912
renato <renatoavila10@hotmail.com>
2022-01-30 00:58:46 -0300

    Limpando codigo

--- index.js
+++ index.js
@@ -13,9 +13,16 @@ const {
 	VoiceConnectionStatus,
 	joinVoiceChannel,
 } = require('@discordjs/voice');
+const { search } = require("yt-search");
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'] });
 const player = createAudioPlayer();
 const prefix = "!";
+const commands = [
+  {
+    name: "play",
+    init: playSong
+  }
+]
 
 
 //client.destroy();
@@ -28,47 +35,59 @@ client.on('ready', () => {
     if(message.author.bot) return;
     if(!message.content.startsWith(prefix)) return;
 
-    if(message.content.startsWith("!play")) {
+    const command = commands.find(c => prefix + c.name == message.content.split(' ')[0]);
 
-      const args = message.content.split(' ').slice(1);
+    if(!command) {
+      message.channel.send("Comando inválido");
+      return;
+    };
 
-      const channel = message.member?.voice.channel
-
-      if(channel) {
-        const connection = await connectToChannel(channel);
-
-        console.log(args.join(' '));
-
-        const r = await yts( args.join(' ') );
-        const id = r.all[0].videoId;
-
-        console.log(r.all[0]);
-
-        stream = ytdl("https://www.youtube.com/watch?v=" + id, {
-          filter: 'audioonly',
-          highWaterMark: 1<<25
-        });
-
-        const resource = createAudioResource(stream, {
-          inputType: StreamType.Arbitrary,
-          inlineVolume: true
-        });
-
-        resource.volume.setVolume(0.5);
-
-        player.play(resource);
-
-        connection.subscribe(player);
-
-        message.channel.send("Tocando hihiih");
-    
-      }
-      
-    }
-
+    command.init(message);
   });
 
 });
+
+async function playSong(message) {
+  //obtem os argumentos apos o comando !play
+  const args = message.content.split(' ').slice(1);
+
+  if(!args.length) {
+    message.channel.send("Erro! Faltou o titulo da música!");
+    return;
+  };
+
+  //recupera o canal de voz conectado pelo usuario que enviou a mensagem
+  const channel = message.member?.voice.channel
+
+  if(channel) {
+    const connection = await connectToChannel(channel);
+    const videos = await yts( args.join(' ') );
+    const id = videos.all[0].videoId;
+
+    console.log(args.join(' '));
+
+    stream = searchVideoById(id);
+    const resource = createAudio(stream);
+    resource.volume.setVolume(1);
+    player.play(resource);
+    connection.subscribe(player);
+    message.channel.send(`Tocando ${videos.all[0].title} \n hihihi`);
+  }
+}
+
+function searchVideoById(id) {
+  return ytdl("https://www.youtube.com/watch?v=" + id, {
+    filter: 'audioonly',
+    highWaterMark: 1<<25
+  });
+}
+
+function createAudio(stream) {
+  return createAudioResource(stream, {
+    inputType: StreamType.Arbitrary,
+    inlineVolume: true
+  });
+}
 
 async function connectToChannel(channel) {
 	const connection = joinVoiceChannel({
